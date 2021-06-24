fdcd29155abd4247111d0bdb0965eeac5341cee4
Von Divino <divinovon@gmail.com>
2021-06-24 16:30:03 +0800

    added theme

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,5 @@ void main() {
   runApp(MaterialApp(
     title: 'Pelago',
     home: HomeScreen(),
-    theme: ThemeData(
-      primaryColor: Color(0xff2E294E),
-      accentColor: Color(0xffBB0E45),
-    ),
   ));
 }
---
+++ lib/ui/pelago_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PelagoTheme {
+  final Color blackColor = Color(0xffF3F2F7);
+  final Color whiteColor = Color(0xffF333333);
+  final Color primaryColorDark = Color(0xff2E294E);
+  final Color primaryColorLight = Color(0xff5056CE);
+  final Color accentDarkColor = Color(0xffBB0E45);
+  final Color accentLightColor = Color(0xffD81F5A);
+}
